6ceb19366e6472216274d6e6d5e194f46e9d524d
Kamran C. Musayev <musayevk@modenis.local>
2019-01-10 10:49:22 +0400

    Components

---
+++ Components/CategoryComponent.cs
@@ -0,0 +1,23 @@
+﻿using BrainStorm.Models.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BrainStorm.Components
+{
+    public class CategoryComponent : ViewComponent
+    {
+        ICategory _category;
+        public CategoryComponent(ICategory category)
+        {
+            _category = category;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View( _category.GetCategories());
+        }
+    }
+}
